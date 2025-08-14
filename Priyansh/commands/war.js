156051dba678e6ae2e51b726d5173bed2652c08c
SK BAAP G <111557947+SK-BAAP-786@users.noreply.github.com>
2025-08-14 15:37:01 +0530

    Update war.js

--- Priyansh/commands/war.js
+++ Priyansh/commands/war.js
@@ -1,8 +1,8 @@
 module.exports.config = {
-    name: "war",
+    name: "Bot fyt",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "MAHTAB AHMAD",
     description: "War in group boxchat",
     commandCategory: "group",
     usages: "bold war",
@@ -20,32 +20,32 @@ module.exports.run = async function({ api, args, Users, event}) {
     var arraytag = [];
         arraytag.push({id: mention});
     var a = function (a) { api.sendMessage(a, event.threadID); }
-a("Listen to your father, kids !");
-setTimeout(() => {a({body: "F*ck your mother" })}, 3000);
-setTimeout(() => {a({body: "You little brats come out to listen to your father curse"})}, 5000);
-setTimeout(() => {a({body: "Quick show the dogs" })}, 7000);
-setTimeout(() => {a({body: "Show your father's soul" })}, 9000);
-setTimeout(() => {a({body: "Do you guys like war so much?" })}, 12000);
-setTimeout(() => {a({body: "Damn you guys too" })}, 15000);
-setTimeout(() => {a({body: "Give your father the age of war" })}, 17000);
-setTimeout(() => {a({body: "Hurry up and curse each other with me" })}, 20000);
-setTimeout(() => {a({body: "Are the bad boys wrinkling their noses up to wage war on your father?" })}, 23000);
-setTimeout(() => {a({body: "I fuck your mother" })}, 25000);
-setTimeout(() => {a({body: "Delicious then yawn your mother up" })}, 28500);
-setTimeout(() => {a({body: "Your father shot you to death by rapping" })}, 31000);
-setTimeout(() => {a({body: "Please age eat me ?" })}, 36000);
-setTimeout(() => {a({body: "If it's delicious, eat your dad" })}, 39000);
+a("𝗢𝗞 𝗕𝗢𝗦𝗦 𝗜𝗠 𝗥𝗘𝗗𝗬 𝗦𝗔𝗕𝗞𝗜 𝗠𝗔𝗔 𝗖𝗛𝗢𝗗 𝗗𝗨𝗡𝗚𝗔 𝗕𝗢𝗦𝗦 𝗧𝗘𝗡𝗦𝗢𝗜𝗡 𝗠𝗔𝗧 𝗟𝗢 𝗔𝗣");
+setTimeout(() => {a({body: "𝐓𝟑𝐑|| 𝐌𝟗 𝐊|| 𝐗𝐇𝐔𝐓𝐓 𝐓𝟗𝐏𝐊𝐊𝐊 𝐁𝟑𝐓𝟗𝟗 𝐁𝐇𝟗𝐆𝐆𝐆 𝐌𝟗𝐓 𝐑𝟗𝟗𝐍𝐃|| 𝐊𝐑 𝐎𝐋𝟗𝐃" })}, 3000);
+setTimeout(() => {a({body: "𝐓𝟑𝐑|| 𝐁𝐇𝟑𝐍 𝐊|| 𝐗𝐇𝐔𝐓 𝐌𝟗𝐑𝐔𝐍 𝐍𝟗𝐌𝟗𝐊 𝐋𝟗𝐆𝟗 𝐊𝟑 𝐒𝟗𝐋𝟑 𝐆𝟗𝐑𝟑𝟑𝐁 "})}, 5000);
+setTimeout(() => {a({body: "𝐓𝟑𝐑|| 𝐌𝟗𝟗 𝐊𝟗𝟗 𝐁𝐇𝐎𝐗𝐃𝟗𝟗 𝐑𝟗𝐍𝐃𝟑𝟑 𝐊𝟑 𝐁𝟗𝐂𝐇𝟑𝟑𝟑 𝐎𝐊𝟗𝐓𝐓 𝐃𝟑𝐊𝐇 𝐁𝐎𝐓 𝐒𝟑 𝐅||𝐘𝐓 𝐊𝐑𝟑𝐆𝟗 𝐓𝐔" })}, 7000);
+setTimeout(() => {a({body: "𝐌𝐔𝐍𝐍𝟗 𝐁𝐎𝐓 𝐒𝟑 𝐅𝐘𝐓 𝐊𝐑𝟑𝐆𝟗𝟗 𝐓𝐔 𝐉𝐇𝟗𝐍𝐓𝐔𝐔 𝐒𝟗𝐋𝟑𝟑 𝐃𝟗𝐋𝟑𝟑𝟑" })}, 9000);
+setTimeout(() => {a({body: "𝐓𝟑𝐑|| 𝐍𝟗𝐍|| 𝐊𝟑 𝐆𝐇𝟗𝐑 𝐉𝟗𝐊𝟑 𝐓𝟑𝐑|| 𝐌𝟗𝟗 𝐊𝐎 𝐗𝐇𝐎𝐃𝐃 𝐃𝐔𝐍𝐆𝟗 𝐁𝟑𝐓𝟗𝟗 ||𝐌 𝐅||𝐓𝐇𝟑𝐑" })}, 12000);
+setTimeout(() => {a({body: "𝐁𝐎𝐓 𝐒𝟑 𝐅𝐘𝐓 𝐊𝐑𝟑𝐆𝟗𝟗 𝐆𝟗𝐑𝟑𝟑𝟑𝐁 𝐒𝟗𝐋𝟑𝟑 𝐉𝐇𝟗𝐍𝐓𝐔𝐔 𝐃𝟗𝐋𝐋𝟑 𝐊𝟑 𝐎𝐋𝟗𝐃𝐃𝐃" })}, 15000);
+setTimeout(() => {a({body: "𝐌𝐔𝐍𝐍𝟗 𝐑𝐑 𝐉𝐎𝐑𝐑 𝐋𝟗𝐆𝟗𝟗 𝐋𝟑𝟑 𝐒𝟗𝐋𝟑𝟑 𝐒𝐔𝐖𝟗𝐑𝐑 𝐊𝟑 𝐎𝟗𝐋𝐃𝐃𝐃𝐃" })}, 17000);
+setTimeout(() => {a({body: "𝟗𝐏𝐊|| 𝐒𝟑𝐒𝐓𝟑𝐑 𝐊𝐎 𝐌𝟑 𝐂𝐇𝐎𝐃 𝐊𝟑 𝐍𝟗𝐍𝐆𝟗 𝐊𝐑 𝐃𝐔𝐍𝐆𝟗𝟗𝟗 𝐌𝟑𝐊𝐎 𝐆𝟗𝐋|| 𝐃𝟑𝐆𝟗 𝐓𝐔 " })}, 20000);
+setTimeout(() => {a({body: "TTTTT🚭AAAAAA👿PPPPPP📌KKKKKK💜 BBBBB🤍EEEEEE💊TTTTT🚭AAAAAA👿 BBBBB🤍HHHHH🖤AAAAAA👿GGGGGG🖤 MMMMM🚀AAAAAA👿TTTTT🚭" })}, 23000);
+setTimeout(() => {a({body: "TTTTT🚭EEEEEE💊RRRRR🔘IIIIII🍷 BBBBB🤍HHHHH🖤EEEEEE💊NNNNNN🤣 KKKKKK💜EEEEEE💊 XXXXX 👰HHHHH🖤UUUUU💣TTTTT🚭" })}, 25000);
+setTimeout(() => {a({body: "SSSSS⚒️AAAAAA👿LLLLLL🔨EEEEEE💊 GGGGGG🖤AAAAAA👿RRRRR🔘EEEEEE💊EEEEEE💊BBBBB🤍 OOOOO🎲KKKKKK💜AAAAAA👿TTTTT🚭 BBBBB🤍AAAAAA👿NNNNNN🤣AAAAAA👿 " })}, 28500);
+setTimeout(() => {a({body: "AAAAAAAAAJJJJJJJJJAAAAAAAAA MMMMMMUUUUUUUUUNNNNNNNNNNNNNNNNAAAAAAAAA BBBBBBBBBHHHHHHHHAAAAAAAAAGGGGGGGG MMMMMMAAAAAAAAATTTTTTTTT" })}, 31000);
+setTimeout(() => {a({body: "𝗧𝗧𝗘𝗘𝗥𝗥𝗜𝗜 𝗕𝗕𝗔𝗔𝗝𝗝𝗜𝗜 𝗞𝗞𝗘𝗘 𝗫𝗫𝗛𝗛𝗨𝗨𝗧𝗧 𝗠𝗠𝗘𝗘 𝗡𝗡𝗔𝗔𝗠𝗠𝗔𝗔𝗞𝗞 𝗛𝗛𝗘𝗘 𝗝𝗝𝗛𝗛𝗔𝗔𝗡𝗡𝗧𝗧𝗨𝗨" })}, 36000);
+setTimeout(() => {a({body: "4444444444JJJJJJJJJJJ4444444444 88888888833333333333777777777774444444444 KKKKKKKKKK4444444444HHHHHHHHH4444444444 8888888888HHHHHHHHH444444444499999999999 RRRRRRRRRR4444444444HHHHHHHHH4444444444 HHHHHHHHH33333333333" })}, 39000);
 setTimeout(() => {a({body: "Before that, please give me a break for 1 minute" })}, 40000);
-setTimeout(() => {a({body: "Please allow me to start" })}, 65000);
-setTimeout(() => {a({body: "First of all, I would like to fuck you from top to bottom" })}, 70000);
-setTimeout(() => {a({body: "I fuck from cunt hole to pussy cleavage" })}, 75000);
-setTimeout(() => {a({body: "The cunt is as big as a buffalo's cunt masturbating a sewer pipe" })}, 80000);
-setTimeout(() => {a({body: "I'm sure 2 guys like me aren't enough to fill your ass hole" })}, 85000);
-setTimeout(() => {a("I'm tired and don't curse anymore")} , 90000);
-setTimeout(() => {a({body: "Come on boss update the lyric, let's continue the war" })}, 95000);
-setTimeout(() => {a({body: "Thank you for listening to me war" })}, 100000);
-setTimeout(() => {a({body: "Goodbye and see you in the next program" })}, 105000);
-setTimeout(() => {a({body: "Good bye 🥺"})} , 115000);
+setTimeout(() => {a({body: "𝑻𝑬𝑹𝑰𝑰𝑰 𝑴𝑨𝑨𝑨 𝑲𝑨𝑨𝑨𝑨 𝑩𝑯𝑶𝑿𝑫𝑨𝑨𝑨𝑨 𝑴𝑨𝑹𝑼𝑼𝑼 𝑺𝑨𝑳𝑬𝑬 𝑴𝑶𝑪𝑯𝑰" })}, 65000);
+setTimeout(() => {a({body: "𝑻𝑼𝑴 𝑯𝑼𝑴𝑺𝑬 𝑭𝒀𝑻𝑻 𝑲𝑹𝑶 𝑮𝑬 𝑩𝑶𝑻 𝑺𝑬 𝑻𝑬𝑹𝑨𝑨 𝑩𝑨𝑨𝑷 𝑯𝑼 𝑮𝑨𝑳𝑰 𝑫𝑬𝑵𝑬 𝑴𝑬𝑬" })}, 70000);
+setTimeout(() => {a({body: "𝑻𝑬𝑹𝑰𝑰 𝑴𝑨𝑨𝑨𝑨 𝑲𝑶 𝑴𝑬 𝑹𝑨𝑨𝑵𝑫𝑰 𝑩𝑨𝑵𝑨 𝑫𝑰𝒀𝑨𝑨 𝑵𝑨𝑨 𝑨𝑩 𝑲𝒀 𝑲𝑹𝑬𝑮𝑨𝑨 𝑳𝑶𝑫𝑬𝑬𝑬" })}, 75000);
+setTimeout(() => {a({body: "𝑻𝑼𝑴 𝑩𝑶𝑻 𝑺𝑬 𝑭𝒀𝑻 𝑵𝑯𝑰 𝑲𝑹 𝑺𝑲𝑻𝑬𝑬𝑬 𝑺𝑨𝑳𝑬𝑬𝑬𝑬 𝑱𝑯𝑨𝑵𝑻𝑼𝑼𝑼𝑼" })}, 80000);
+setTimeout(() => {a({body: "𝑻𝑬𝑹𝑰 𝑪𝑯𝑶𝑻𝑰 𝑩𝑯𝑬𝑵 𝑲𝑰 𝑪𝑯𝑼𝑪𝑯𝑰 𝑫𝑨𝑩𝑨𝑨𝑨 𝑳𝑼𝑵𝑮𝑨𝑨 𝑨𝑱 𝑴𝑬𝑬𝑬😀😀" })}, 85000);
+setTimeout(() => {a({body: "73R!! 8HRN K444 K414 K414 8H0XD44 DRKH K3 M3 D4R 9Y44 54133")} , 90000);
+setTimeout(() => {a({body: "4J44 837444 8H49 N44 NH!! 4PK!! 84J!! K!! XHU77 131UN944" })}, 95000);
+setTimeout(() => {a({body: "74PKKKK M3R444 8474444 K4H4 8H4999 9Y444 7U" })}, 100000);
+setTimeout(() => {a({body: "AB GALI DIYAAA TO THIKK NA HOGAA AB METOR KHTAMM KRTA HU ME 😙" })}, 105000);
+setTimeout(() => {a({body: "IM MASOOM MEKO AB GALI NA DENA  🥺"})} , 115000);
 
 
 
