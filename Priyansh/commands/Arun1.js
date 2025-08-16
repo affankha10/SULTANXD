2c0233757eedf4e40443d501197f57565fb09b8f
affankha10 <saifkhan376376376@gmail.com>
2025-08-16 12:33:40 +0530

    Update Arun1.js

--- Priyansh/commands/Arun1.js
+++ Priyansh/commands/Arun1.js
@@ -1,9 +1,9 @@
  const fs = require("fs");
 module.exports.config = {
-	name: "mahtab",
+	name: "sultan",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "Sultan", 
+	credits: "sultan", 
 	description: "no prefix",
 	commandCategory: "No command marks needed",
 	usages: "...",
